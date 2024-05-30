3e5502dae540657d85ffc80b01a790ed55d7190d
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-30 11:46:54 -0700

    added screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,14 @@ const config: Config = {
       },
       colors: {
         textColor: "#FFF5EE"
-      }
+      },
+      screens: {
+        smm: "300px",
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
     },
   },
   plugins: [],
