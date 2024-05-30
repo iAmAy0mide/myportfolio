fcaee82b20c4d33e341fce2af6037405fb238efd
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-30 13:03:08 -0700

    ups

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,25 @@
-import Navigations from '@/components/Navigations'
-import React from 'react'
+import Illustrations from '@/components/Illustrations';
+import Navigations from '@/components/Navigations';
+import AboutIllus from "/public/about.svg";
 
 const page = () => {
   return (
     <main className='main'>
-      <h1 className='slide-in-blurred-top'>About</h1>
+      <div className="text-center pt-[3rem]  md:pt-[4rem] z-10 bg-bgColor w-[400px] mx-auto">
+        <h1 className='slide-in-blurred-top text-center text-[3rem] text-textColor font-extrabold shadow'>About Me</h1>
+        <p className="">
+        I'm [Your Name], a full-stack developer driven by a passion for transforming ideas into digital reality. With a focus on both front-end finesse and back-end robustness, I thrive in the creative process of building intuitive, user-centric websites and applications.
+        </p>
+      </div>
       <Navigations />
+      <Illustrations 
+        alt='a man siiting in office - illusrating info about the developer'
+        height={undefined}
+        width={undefined}
+        src={AboutIllus}
+      />
     </main>
   )
 }
 
-export default page
+export default page;
