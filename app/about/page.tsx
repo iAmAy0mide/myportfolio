a7546a338b1a32d20b4470f15c2ca912fa2814fc
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-30 14:18:56 -0700

    ups

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,11 +4,31 @@ import AboutIllus from "/public/about.svg";
 
 const page = () => {
   return (
-    <main className='main'>
-      <div className="text-center pt-[3rem]  md:pt-[4rem] z-10 bg-bgColor w-[400px] mx-auto">
-        <h1 className='slide-in-blurred-top text-center text-[3rem] text-textColor font-extrabold shadow'>About Me</h1>
+    <main className='min-h-screen'>
+      <div className="text-center pt-[3rem]  md:pt-[4rem] z-10 sm:w-2/3 md:w-1/2 mx-auto md:px-8">
+        <span className='absolute text-stroke-tr -z-10 left-10 top-5 text-[4rem] font-extrabold rotate-45'>ME</span>
+        <span className='absolute -z-10 top-[10rem] hidden sm:block left-20 bottom-10 text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 bottom-20 right-20 text-stroke-tr text-[4rem] font-extrabold rotate-45'>ME</span>
+        <span className='absolute -z-10 bottom-[10rem] hidden sm:block left-10 text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 top-[15rem] left-[23rem] text-stroke-tr text-[4rem] font-extrabold rotate-45'>ME</span>
+        <span className='absolute -z-10 top-[18rem] left-[18rem] text-stroke-tr text-[4rem] font-extrabold rotate-45'>ME</span>
+        <span className='absolute -z-10 -top-[15rem] -left-[15rem]  text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 right-[15rem] hidden sm:block bottom-[15rem] text-stroke-tr text-[4rem] font-extrabold rotate-45'>ME</span>
+        <span className='absolute -z-10 right-[14rem] bottom-[18rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 right-[18rem] hidden sm:block bottom-[8rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 right-[25rem] bottom-[30rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 left-[25rem] hidden sm:block top-[30rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 right-[30rem] bottom-[30rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 top-[3rem] hidden sm:block left-[30rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 bottom-[3rem] right-[30rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 right-[35rem] bottom-[10rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 right-[3rem] top-[2rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 left-[3rem] bottom-[2rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 left-[10rem] bottom-[5rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <span className='absolute -z-10 right-[3rem] bottom-[2rem] text-stroke-tr text-[4rem] font-extrabold -rotate-45'>ME</span>
+        <h1 className='slide-in-blurred-top text-center text-[3rem] text-textColor font-extrabold'>About Me</h1>
         <p className="">
-        I'm [Your Name], a full-stack developer driven by a passion for transforming ideas into digital reality. With a focus on both front-end finesse and back-end robustness, I thrive in the creative process of building intuitive, user-centric websites and applications.
+        I'm Ayomide, a full-stack developer driven by a passion for transforming ideas into digital reality. With a focus on both front-end finesse and back-end robustness, I thrive in the creative process of building intuitive, user-centric websites and applications.
         </p>
       </div>
       <Navigations />
--- components/Illustrations.tsx
+++ components/Illustrations.tsx
@@ -11,7 +11,7 @@ const Illustrations: React.FC<IllustrationProp> = ({ alt, height, width, src}) =
         height={height} 
         width={width} 
         src={src} 
-        className="-z-10 right-1 bottom-[8rem] smm:bottom-6 sm:bottom-1 absolute w-[30rem] md:bottom-1 md:right-10 md:w-[32rem] xl:w-[37rem] xl:right-2"
+        className="-z-10 right-1 bottom-[8rem] smm:bottom-6 sm:bottom-1 absolute w-[30rem] md:bottom-4 md:right-10 md:w-[32rem] xl:w-[37rem] xl:right-2"
       />
     </>
   )
@@ -23,5 +23,5 @@ interface IllustrationProp {
     alt: string;
     height?: number | `${number}` | undefined;
     width?: number | `${number}` | undefined;
-    src: string | StaticImport;
+    src: string | StaticImport | any;
 }
