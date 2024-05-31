b20f511a3b77bc0d3a2412a0eab5d3c8f7f58cff
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-31 01:51:02 -0700

    className update

--- components/Illustrations.tsx
+++ components/Illustrations.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 import React from 'react'
 
-const Illustrations: React.FC<IllustrationProp> = ({ alt, height, width, src}) => {
+const Illustrations: React.FC<IllustrationProp> = ({ alt, height, width, src, className}) => {
   return (
     <>
       <Image 
@@ -11,7 +11,7 @@ const Illustrations: React.FC<IllustrationProp> = ({ alt, height, width, src}) =
         height={height} 
         width={width} 
         src={src} 
-        className="-z-10 right-1 bottom-[8rem] smm:bottom-6 sm:bottom-1 absolute w-[30rem] md:bottom-4 md:right-10 md:w-[32rem] xl:w-[37rem] xl:right-2"
+        className={className}
       />
     </>
   )
@@ -24,4 +24,5 @@ interface IllustrationProp {
     height?: number | `${number}` | undefined;
     width?: number | `${number}` | undefined;
     src: string | StaticImport | any;
+    className: string;
 }
