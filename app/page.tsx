7e3c69d0440fe0f1499d700815eb35f938ee0d61
iAmAyomide <ayomidekenny952@gmail.com>
2024-05-31 01:51:28 -0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Illustrations from "@/components/Illustrations";
 function Home() {
 
   return (
-    <main className="min-h-screen relative tilt-in-fwd-tl">
+    <main className="tilt-in-fwd-tl">
       <div className="text-center pt-[3rem]  md:pt-[4rem] z-10">
         <h1 className="text-[3rem] text-textColor font-extrabold neumorphic">Ayomide Enirayetan</h1>
         <h2 className="text-[1.4rem] text-textColor">Full Stack Web Developer</h2>
@@ -23,6 +23,7 @@ function Home() {
         height={undefined} 
         width={undefined} 
         src={Innovation} 
+        className="-z-10 right-1 bottom-[8rem] smm:bottom-6 sm:bottom-1 absolute w-[30rem] md:bottom-4 md:right-10 md:w-[32rem] xl:w-[37rem] xl:right-2"
        />
     </main>
   )
