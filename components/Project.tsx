702a81fa6951618a5906acbf93ee7bdf646d4b82
iAmAyomide <ayomidekenny952@gmail.com>
2024-06-02 20:30:42 -0700

    ups

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Project = () => {
   return (
     <div>
-      
+      <div className="">
+        {/* <Image /> */}
+        <div className="absolute"></div>
+      </div>
+      <div className="">
+        <h1></h1>
+        <p></p>
+        <button>Preview</button>
+      </div>
     </div>
   )
 }
