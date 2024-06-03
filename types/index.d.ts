9751896eacf57411e7fe432c6bdc5acb134908c9
iAmAyomide <ayomidekenny952@gmail.com>
2024-06-02 19:09:49 -0700

    types

---
+++ types/index.d.ts
@@ -0,0 +1,15 @@
+declare interface IErrorInputs  {
+    firstName: string;
+    lastName: string;
+    email: string;
+    subject: string;
+    description: string;
+}
+
+declare type IFormData = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    subject: string;
+    description: string;
+}
