3e02d4194cfa42f9a4629a0c38969bb746714094
iAmAyomide <ayomidekenny952@gmail.com>
2024-06-05 09:14:53 -0700

    z-index update for nav

--- components/Navigations.tsx
+++ components/Navigations.tsx
@@ -7,7 +7,7 @@ const Navigations: React.FC<INav> = ({ isModal }) => {
     const pathname = usePathname();
 
   return (
-    <nav  className={`fixed text-white bottom-10 left-4 md:left-16 ${isModal ? '-z-10' : 'z-30'}`}>
+    <nav  className={`fixed text-white bottom-10 left-4 md:left-16 ${isModal ? 'z-1' : 'z-30'}`}>
       <div>
         <Link 
           href="/" 
@@ -25,8 +25,8 @@ const Navigations: React.FC<INav> = ({ isModal }) => {
         </div>
         <div>
           <Link 
-          href="/skills" 
-          className={`block text-textColor text-[1.2rem] py-2 hover:px-2 light-up hover:pl-2 transition-all duration-500 ${pathname === "/skills" ? 'inner-shadow pl-2' : '' }`}
+          href="/techs" 
+          className={`block text-textColor text-[1.2rem] py-2 hover:px-2 light-up hover:pl-2 transition-all duration-500 ${pathname === "/techs" ? 'inner-shadow pl-2' : '' }`}
           >Techs</Link>
           <div className="h-[2px] w-[5.5rem] bg-textColor"></div>
         </div>
