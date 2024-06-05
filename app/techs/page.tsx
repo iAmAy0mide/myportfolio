92a35a75672a554428af489115ee0e4fad16181b
iAmAyomide <ayomidekenny952@gmail.com>
2024-06-05 09:14:10 -0700

    tecchs/ skills page ups

---
+++ app/techs/page.tsx
@@ -0,0 +1,23 @@
+import Navigations from '@/components/Navigations'
+import React from 'react';
+import html from "/public/html.png";
+import css from "/public/css3.png";
+import ts from "/public/typescript.webp";
+import js from "/public/javascript.png";
+import react from "/public/react.png";
+import nextjs from "/public/nextjs.png";
+import nodejs from "/public/nodejs.png";
+import tailwind from "/public/tailwind.png";
+import express from "/public/express.jpg";
+import sanity from "/public/sanity.png";
+
+const Techs = () => {
+  return (
+    <main className='mx-auto'>
+      Skills
+      <Navigations />
+    </main>
+  )
+}
+
+export default Techs
