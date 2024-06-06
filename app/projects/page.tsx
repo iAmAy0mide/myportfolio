bb76be701671dbee1163857c98867074ce0c5f16
iAmAyomide <ayomidekenny952@gmail.com>
2024-06-06 15:31:13 -0700

    ups

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,33 +1,32 @@
-"use client"
+"use client";
 import React from 'react';
 import Navigations from '@/components/Navigations';
-import Image from 'next/image';
 import proto from "/public/proto.png";
 import Bashorun from "/public/bashorun.png";
-import ProjectModal from '@/components/ProjectModal';
 import Project from '@/components/Project';
-// import Project from '@/components/Project';
 
 
 const Projects = () => {
   const [modalIsOpen, setModalIsOpen] = React.useState<boolean>(false);
 
   return (
-    <main className='mt-6 '>
+    <main className='mt-6 mx-4 w-full space-y-4'>
        <Project
           alt='a representative image of Sevlar - a bank mangement web app'
           logoAlt='a representative logo of Sevlar - a bank mangement web app'
           src={proto} 
           logoSrc={Bashorun}
           projectName={'Bashorunolu Legal Website'} 
-          description={'This website is a law firm greatest.'}/>
+          description={'This website is a law firm greatest.'}
+        />
        <Project
           alt='a representative image of Sevlar - a bank mangement web app'
           logoAlt='a representative logo of Sevlar - a bank mangement web app'
           src={proto} 
           logoSrc={Bashorun} 
           projectName={'Bashorunolu Legal Website'} 
-          description={'This website is a law firm greatest.'}/>
+          description={'This website is a law firm greatest.'}
+        />
       <Navigations isModal={true} />
     </main>
   )
