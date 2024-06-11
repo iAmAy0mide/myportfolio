b671536b098f4149142f48a4274f0a991492c929
iAmAy0mide <ayomidekenny952@gmail.com>
2024-06-10 21:51:31 -0700

    updated metadata

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,7 +7,7 @@ import Project from '@/components/Project';
 const Projects = () => {
 
   return (
-    <main id='projects' className=' gap-4 mt-4 grid  grid-cols-1 sm:grid-cols-2 md:grid-col-3 px-4 md:px-16'>
+    <main id='projects' className=' gap-4 mt-4 grid  grid-cols-1 sm:grid-cols-2 md:grid-col-3 px-4 md:px-16 '>
     {/* <main id='projects' className='mt-6 mx-4 w-full space-y-4'> */}
        <Project
           alt='a representative image of Sevlar - a bank mangement web app'
